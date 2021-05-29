4886715181f68a770e0f45a9dff783ad180b79cc
Kartik Singhal <kartiksinghal122@gmail.com>
2021-05-29 15:08:11 +0530

    Intialise weights to avoid vanishing/exploding gradients, functionality for validation data, new classifation using dummy data
    fix

--- keras/layers/Dense.py
+++ keras/layers/Dense.py
@@ -1,28 +1,36 @@
 """
 Attributes:
+n_units: int
+    Number of units in the layer
 W: n_units_in_prev_layer * n_units_in_this_layer numpy array 
     Weights of this layer
 b: n_units_in_current_layer * 1 numpy array 
     biases of this layer
-Z: n_units * m dimensional array
-    Z = WX + b for each unit for each data point
-A: n_units * m dimensional array
+activation: activation object
+activation_name: string
+    Name of activation function
+A: (temporarily used during backprop): n_units * m dimensional array
     activation of Z
 """
 class Dense:
     """
     Parameters:
     activation: string
-        "relu", "sigmoid"
+        "relu", "sigmoid", "linear"
     """
     def __init__(self, n_units, activation="linear"):
         self.n_units = n_units
         if(activation == "relu"):
             from keras.activations.relu import relu
+            self.activation_name = "relu"
             self.activation = relu()
+            
         elif(activation == "sigmoid"):
             from keras.activations.sigmoid import sigmoid
+            self.activation_name = "sigmoid"
+            
             self.activation = sigmoid()
         elif(activation == "linear"):
             from keras.activations.linear import linear
+            self.activation_name = "linear"
             self.activation = linear()
--- keras/models/Sequential.py
+++ keras/models/Sequential.py
@@ -13,7 +13,7 @@ class Sequential:
     def __init__(self, layers):
         self.layers = layers
         self.n_layers = len(self.layers)
-        self.loss_history = np.array([])
+        self.history = {}
     
     """
     compile
@@ -25,8 +25,23 @@ class Sequential:
     def compile(self, loss, learning_rate=0.1):
         #initialise weights
         for i in range(1, len(self.layers)):
-            self.layers[i].W = np.random.randn(self.layers[i].n_units, self.layers[i-1].n_units) * 0.01
-            self.layers[i].b = np.random.randn(self.layers[i].n_units, 1) * 0.01
+            activation_name = self.layers[i].activation_name
+            # Check in case someone applies activation on input layer
+            if i>0:
+                n_units_in_prev_layer = self.layers[i-1].n_units
+                
+            if activation_name == "relu":
+                self.layers[i].W = np.random.randn(self.layers[i].n_units, self.layers[i-1].n_units)* \
+                                                                            np.sqrt(2/n_units_in_prev_layer)
+                
+            elif activation_name == "sigmoid":
+                self.layers[i].W = np.random.randn(self.layers[i].n_units, self.layers[i-1].n_units) * \
+                                                                            np.sqrt(1/n_units_in_prev_layer)
+                
+            elif activation_name == "linear":
+                self.layers[i].W = np.random.randn(self.layers[i].n_units, self.layers[i-1].n_units)
+                
+            self.layers[i].b = np.zeros((self.layers[i].n_units, 1))
         
         self.learning_rate = learning_rate
         if(loss == "binary_crossentropy"):
@@ -36,12 +51,10 @@ class Sequential:
             
     def forward_propagation(self, X):
         a = X
-        self.layers[0].Z = X
         self.layers[0].A = X
         for i in range(1, self.n_layers):
             z = np.dot(self.layers[i].W, a) + self.layers[i].b
             a = self.layers[i].activation.activate(z)
-            self.layers[i].Z = z
             self.layers[i].A = a   
         return a
     
@@ -59,12 +72,21 @@ class Sequential:
             dz = np.multiply(np.dot(self.layers[i].W.T, dz), \
                              self.layers[i - 1].activation.derivative(activation_prev_layer))
              
-    def fit(self, X, Y, epochs=100):
+    def fit(self, X, Y, X_val=np.array([]), Y_val=np.array([]), epochs=100, verbose=1):
+        self.history["train_loss"] = np.array([])
+        if X_val.size != 0:
+            self.history["val_loss"] = np.array([])
+            
         for i in range(epochs):
             self.forward_propagation(X)
             self.backward_propagation(Y)
-            output = self.layers[self.n_layers - 1].A
-            self.loss_history = np.append(self.loss_history, self.loss.calculateLoss(output, Y))
+            train_output = self.layers[self.n_layers - 1].A
+            self.history["train_loss"] = np.append(self.history["train_loss"], self.loss.calculateLoss(train_output, Y))
             
+            if X_val.size != 0:
+                val_output = self.forward_propagation(X_val)
+                self.history["val_loss"] = np.append(self.history["val_loss"], self.loss.calculateLoss(val_output, Y_val))
+
+        
     def predict(self, X):
         return self.forward_propagation(X)
